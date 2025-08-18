e06e05ed98a213f10df0810c144304f4e49096ba
Caring Person <kuuumz@gmail.com>
2025-08-18 23:07:37 +0000

    ok i find i have to refresh the app for the popup to show otherwise i ge

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,9 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
+import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
+import { getStorage, type FirebaseStorage } from "firebase/storage";
+import { getFirestore, type Firestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, OAuthProvider, type Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,20 +15,31 @@ const firebaseConfig = {
   "appId": "1:31517443304:web:e6cbc2189a4eeaa489355d"
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const storage = getStorage(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
+// Singleton pattern to ensure Firebase is initialized only once
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
+let storage: FirebaseStorage;
+let googleProvider: GoogleAuthProvider;
+let microsoftProvider: OAuthProvider;
 
-const googleProvider = new GoogleAuthProvider();
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
+}
+
+auth = getAuth(app);
+db = getFirestore(app);
+storage = getStorage(app);
+
+googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-  'authDomain': 'recipebox-ifiwn.firebaseapp.com'
+  'prompt': 'select_account'
 });
 
-const microsoftProvider = new OAuthProvider('microsoft.com');
+microsoftProvider = new OAuthProvider('microsoft.com');
 microsoftProvider.setCustomParameters({
-  // Use the v2.0 endpoint for Microsoft accounts (personal & work/school)
   tenant: 'common',
 });
 
